a72226f5484de443bb225238a6f42adc3f58d6e6
NBalokha <nba@dao.as>
2025-09-24 13:01:24 +0200

    BREAK STUFF

--- applications/nazAPIi/main.py
+++ applications/nazAPIi/main.py
@@ -9,5 +9,16 @@ async def get_all_users():
     return database.get_all_users()
 
 @app.get("/user/{search_term}")
-async def get_user_by_search(search_term: str):
+async def get_user_by_search(search_term: str):da¨pådkaøpodu ba¨09døadqa
+def
+ daid 'åaid
+ a di
+    0 D
+            QI 
+            DeprecationWarningSD IsADirectoryErrorJU         'D
+            ADO ArithmeticErrorADA 
+            A
+            D ArithmeticErrorDA
+            DP A 
+            AIOHDOÅQIYDÅ091 90AJ'D
     return database.get_user_by_search(search_term)
